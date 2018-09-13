236cc9d2d2d3f6a932013d0ce9483ad46fffe1c7
Mersdl <794469928@qq.com>
2018-09-13 15:16:27 +0800

    Initial commit

---
+++ web2py/applications/main/controllers/hello.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: shengdaolin
+@license: (C) Copyright 2015-2018, www.findAll.com
+@contact: 794469928@qq.com
+@software: pycharm
+@file: hello.py
+@time: 2018/9/13 15:05
+@desc:
+'''
+
+
+def  index() :
+    return ("Hello Python !")
+
+def  say() :
+    return ("Hello Web2Py !")
---
+++ web2py/parameters_8000.py
@@ -0,0 +1,1 @@
+password="pbkdf2(1000,20,sha512)$8acccc56c04bfef4$dcbfbc11baef498454e1320cb8f36740a9216742"
